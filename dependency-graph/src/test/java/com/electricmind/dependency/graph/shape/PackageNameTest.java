3417903f9dd7b9a5d2eed2f62f52f216e340bcb6
BC Holmes <bc.holmes@electricmind.com>
2025-07-30 11:15:51 -0400

    Minor test revision

--- dependency-graph/src/test/java/com/electricmind/dependency/graph/shape/PackageNameTest.java
+++ dependency-graph/src/test/java/com/electricmind/dependency/graph/shape/PackageNameTest.java
@@ -1,30 +1,37 @@
 package com.electricmind.dependency.graph.shape;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class PackageNameTest extends TestCase {
+import org.junit.Test;
 
+public class PackageNameTest {
+
+	@Test
 	public void testGetDepth() throws Exception {
 		assertEquals("ca.intelliware.example", 3, new PackageName("ca.intelliware.example").getDepth());
 	}
+	@Test
 	public void testGetCommonPrefix() throws Exception {
 		PackageName example = new PackageName("ca.intelliware.example");
 		PackageName exampleModel = new PackageName("ca.intelliware.example.model");
 		assertEquals("prefix", new PackageName("ca.intelliware.example"), example.getCommonPrefix(exampleModel));
 	}
 	
+	@Test
 	public void testGetCommonPrefix2() throws Exception {
 		PackageName exampleService = new PackageName("ca.intelliware.example.service");
 		PackageName exampleModel = new PackageName("ca.intelliware.example.model");
 		assertEquals("prefix", new PackageName("ca.intelliware.example"), exampleService.getCommonPrefix(exampleModel));
 	}
 	
+	@Test
 	public void testRemovePrefix() throws Exception {
 		PackageName exampleService = new PackageName("ca.intelliware.example.service");
 		PackageName prefix = new PackageName("ca.intelliware.example");
 		assertEquals("result", new PackageName("service"), exampleService.removePrefix(prefix));
 	}
 	
+	@Test
 	public void testGetCommonPrefixWithNoCommonality() throws Exception {
 		PackageName exampleService = new PackageName("com.intware.fred.service");
 		PackageName exampleModel = new PackageName("ca.intelliware.example.model");
